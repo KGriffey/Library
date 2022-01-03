bdee41b83d624172120d3c90b9efe4a9a7f7dcd4
KGriffey <griffey.kyle@gmail.com>
2022-01-03 15:13:44 -0700

    Refactored code, improved modal and event handlers

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-/* Library adn Book Object/Methods */
+/* Library and Book Object/Methods */
 function Library() {
     this.Collection = [];
 
-    this.addBookToLibrary = function (Book) {
+    this.addBook = function (Book) {
         this.Collection.push(Book);
+    }
+
+    this.hasBook = function(title, author){
+        for(let i = 0; i < this.Collection.length; i++){
+            if(this.Collection[i].title.toLowerCase() == title.toLowerCase() 
+            && this.Collection[i].author.toLowerCase() == author.toLowerCase()){
+                return true;
+            }
+        }
+        return false;
     }
 }
 
@@ -14,31 +24,8 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-/* New Book Overlay and Button*/
-function initAddBookBtn() {
-    const addBookBtn = document.querySelector("#addBookBtn button");
-    addBookBtn.addEventListener("click", overlayOn);
-}
-
-function overlayOn(e) {
-    e.stopPropagation();
-    document.getElementById("addBookOverlay").style.display = "block";
-    document.addEventListener("click", overlayExit);
-}
-
-function overlayExit(e) {
-    if (!e.target.closest(".overlay")) {
-        overlayOff();
-    }
-}
-
-function overlayOff() {
-    document.getElementById("addBookOverlay").style.display = "none";
-    document.removeEventListener("click", overlayExit);
-}
-
 /* Book Display */
-function displayBooks(Library) {
+function displayLibrary() {
     //Remove previous display of books
     const books = document.getElementById("books");
     while(books.firstChild){
@@ -46,7 +33,7 @@ function displayBooks(Library) {
     }
 
     //Update the display with all current books in the library
-    Library.Collection.forEach(book => {
+    myLib.Collection.forEach(book => {
         const bookCard = document.createElement("div");
         bookCard.setAttribute("class", "book");
 
@@ -74,44 +61,74 @@ function displayBooks(Library) {
     });
 }
 
-/* Forms */
-function submitFormEventListener() {
-    const form = document.getElementById("addBookForm");
-    form.addEventListener("submit", parseForm);
+function updateLibrary(){
+    //Check the entered info for validity before creating the new book.
+    //Return error message if info needs fixing and exit function.
+    const bookInfo = getBookInfo();
+    if(!isBookInfoValid(bookInfo)){
+        return;
+    }
+    let newBook = new Book(bookInfo[0],bookInfo[1],bookInfo[2],bookInfo[3]);
+
+    //Add the book to the user's library
+    myLib.addBook(newBook);
+
+    //Update the library display
+    displayLibrary();
+
+    //Remove the modal
+    modalBackground.classList.remove("modal-background--active");
 }
 
-function parseForm(e) {
-    //Prevent the submit button from refreshing the page
-    e.preventDefault();
+function getBookInfo() {
+    //Get new book info from input fields
+    const title = document.querySelector('[name="title"]').value;
+    const author = document.querySelector('[name="author"]').value;
+    const pages = document.querySelector('[name="pages"]').value;
+    const isRead = document.querySelector('[name="isRead"]').checked;
+    const bookInfo = [title,author,pages,isRead];
 
-    //Parse the form data for the new book entry
-    const title = this.elements["title"].value;
-    const author = this.elements["author"].value;
-    const pages = this.elements["pages"].value;
-    const isRead = this.elements["isRead"].checked;
-    let book = new Book(title, author, pages, isRead);
-
-    //Add book to library and update the display
-    myLib.addBookToLibrary(book);
-    displayBooks(myLib);
-
-    //Reset the form and turn off the overlay
-    this.reset();
-    overlayOff();
-
+    //Return info as an array
+    return bookInfo;
 }
 
+function isBookInfoValid(bookInfo) {
+    if(bookInfo[0] == "" || bookInfo[1] == "" || bookInfo[2] == "") {
+        //Check for a blank field
+        alert("All fields must be filled.")
+        return false;
+    } else if (bookInfo[2] < 1 || !Number.isInteger(Number(bookInfo[2]))) {
+        //Check that page count is a positive integer
+        alert("Page count invalid.")
+        return false;
+    } else if (myLib.hasBook(bookInfo[0],bookInfo[1])) {
+        //Check if the same title/author combination exists in the library
+        alert("This book is already in your collection!")
+        return false;
+    } else {
+        return true;
+    }
+}
+
+const modalBackground = document.querySelector(".modal-background");
+const addBookBtn = document.getElementById("addBookBtn");
+const modalExitBtn = document.querySelector(".modal-exit");
+const modalSubmitBtn = document.querySelector(".submit");
+
+modalExitBtn.addEventListener("click", function() {
+    modalBackground.classList.remove("modal-background--active");
+});
+
+addBookBtn.addEventListener("click", function() {
+    modalBackground.classList.add("modal-background--active");
+});
+
+modalSubmitBtn.addEventListener("click", updateLibrary);
 
 const Inferno = new Book("Inferno", "Dante", 300, false);
 const Test = new Book("Test", "Myself", 200, true);
-const Another = new Book("Another", "Myself", 100, true);
-
 let myLib = new Library();
-myLib.addBookToLibrary(Inferno);
-myLib.addBookToLibrary(Test);
-myLib.addBookToLibrary(Another);
+myLib.addBook(Inferno);
+myLib.addBook(Test);
 
-displayBooks(myLib);
-initAddBookBtn();
-submitFormEventListener();
-
+displayLibrary();
